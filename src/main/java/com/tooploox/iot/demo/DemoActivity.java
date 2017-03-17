677f2070a64eba5d9c118d5f05245fd8d0296f86
Maksym Dybarskyi <maksym.dybarskyi@tooploox.com>
2017-03-17 20:01:47 +0100

    Refactor method

--- src/main/java/com/tooploox/iot/demo/DemoActivity.java
+++ src/main/java/com/tooploox/iot/demo/DemoActivity.java
@@ -3,6 +3,7 @@ package com.tooploox.iot.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.SeekBar;
+import android.widget.Switch;
 
 import com.google.android.things.pio.PeripheralManagerService;
 
@@ -19,14 +20,28 @@ public class DemoActivity extends Activity {
     @BindView(R.id.frequency)
     SeekBar seekBar;
 
+    @BindView(R.id.enable)
+    Switch switcher;
+
     PeripheralManagerService service = new PeripheralManagerService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        initUI();
+        initPeriphery();
+    }
+
+    private void initPeriphery() {
+
+    }
+
+    private void initUI() {
         setContentView(R.layout.activity);
+
         ButterKnife.bind(this);
+        switcher.setOnCheckedChangeListener((buttonView, isChecked) -> onEnableChecked(isChecked));
         seekBar.setOnSeekBarChangeListener(new SimpleSeekbarListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -35,12 +50,11 @@ public class DemoActivity extends Activity {
         });
     }
 
-    @OnCheckedChanged(R.id.enable)
-    void onEnableChecked(boolean checked) {
+    private void onEnableChecked(boolean checked) {
 
     }
 
-    void onFrequencyChanged(int progress) {
+    private void onFrequencyChanged(int progress) {
 
     }
 }
