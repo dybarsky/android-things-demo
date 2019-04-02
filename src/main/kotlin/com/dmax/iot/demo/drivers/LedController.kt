c312fb542779f5270e36bb3eeed5ca12dc013d7a
Maksym Dybarskyi <maxim.dybarskyy@gmail.comm>
2019-04-02 21:29:37 +0200

    Fix led controller

--- src/main/kotlin/com/dmax/iot/demo/drivers/LedController.kt
+++ src/main/kotlin/com/dmax/iot/demo/drivers/LedController.kt
@@ -2,21 +2,37 @@ package com.dmax.iot.demo.drivers
 
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.PeripheralManager
+import java.io.IOException
 
 /**
  * Created by mdy on 3/22/17.
  */
 class LedController(peripheral: PeripheralManager, pin: String) {
 
-    private val gpio: Gpio = peripheral.openGpio(pin)
-
-    init {
-        gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+    private val gpio: Gpio? = try {
+        peripheral.openGpio(pin)
+    } catch (_: IOException) {
+        null
     }
 
-    fun on() { gpio.value = true }
+    init {
+        gpio?.apply {
+            setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+            setActiveType(Gpio.ACTIVE_LOW)
+        }
+    }
 
-    fun off() { gpio.value = false }
+    fun on() {
+        gpio?.value = true
+    }
 
-    fun close() { gpio.close() }
+    fun off() {
+        gpio?.value = false
+    }
+
+    fun close() {
+        try {
+            gpio?.close()
+        } catch (_: IOException) {}
+    }
 }
