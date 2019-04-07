6266ea815bf6c400eb13a70a85f029718f0df176
Maksym Dybarskyi <maxim.dybarskyy@gmail.comm>
2019-04-08 01:03:37 +0200

    Implement i2c

--- src/main/kotlin/com/dmax/iot/demo/activities/I2cActivity.kt
+++ src/main/kotlin/com/dmax/iot/demo/activities/I2cActivity.kt
@@ -1,21 +1,38 @@
 package com.dmax.iot.demo.activities
 
+import com.dmax.iot.demo.drivers.PCA9685
 import com.google.android.things.pio.I2cDevice
 import java.io.IOException
 
-private const val I2C_DEVICE_NAME: String = "i2c"
-private const val I2C_ADDRESS: Int = 0x00
-
+private const val I2C_ADDRESS: Int = 0x40
 
 open class I2cActivity : IotActivity() {
+
+    private companion object {
+        /*
+        min servo time = 500 us
+        max servo time = 2400 us
+        ticks per second = 4096 * 50 = 204800
+        min ticks = 204800 / 1000000 * 500 = 103
+        max ticks = 204800 / 1000000 * 2400 = 492
+        one degree time = (492 - 103) / 180 = ~2
+        */
+
+        private const val MIN_TIME = 103
+        private const val MAX_TIME = 492
+        private const val ONE_DEGREE_TIME = 2
+    }
 
     private var i2c: I2cDevice? = null
 
     override fun initPeriphery() {
-        i2c = try {
-            peripheral.openI2cDevice(I2C_DEVICE_NAME, I2C_ADDRESS)
-        } catch (_: IOException) {
-            null
+        peripheral.i2cBusList.firstOrNull()?.let {
+            i2c = try {
+                peripheral.openI2cDevice(it, I2C_ADDRESS)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                null
+            }
         }
     }
 
@@ -26,6 +43,12 @@ open class I2cActivity : IotActivity() {
     }
 
     override fun doAction() {
-        i2c?.
+        i2c?.let {
+            PCA9685(it).apply {
+                reset()
+                setPwmFrequency(50)
+                setPwmValue(0, 0)
+            }
+        }
     }
 }
---
+++ src/main/kotlin/com/dmax/iot/demo/drivers/PCA9685.kt
@@ -0,0 +1,83 @@
+package com.dmax.iot.demo.drivers
+
+import com.google.android.things.pio.I2cDevice
+import kotlin.experimental.and
+import kotlin.experimental.or
+
+private const val PCA9685_MODE1 = 0x0
+private const val PCA9685_PRESCALE = 0xFE
+
+private const val LED0_ON_L = 0x6
+private const val LED0_ON_H = 0x7
+private const val LED0_OFF_L = 0x8
+private const val LED0_OFF_H = 0x9
+
+private const val LED1_ON_L = 0x0a
+private const val LED1_ON_H = 0x0b
+private const val LED1_OFF_L = 0x0c
+private const val LED1_OFF_H = 0x0d
+
+private const val PCA9685_25Mhz_TICKS = 25000000f
+
+class PCA9685(val i2c: I2cDevice) {
+
+    fun reset() {
+        val value: Byte = 0x08
+        println("reset")
+        i2c.writeRegByte(PCA9685_MODE1, value)
+    }
+
+    fun setPwmFrequency(frequency: Byte) {
+        val freq = frequency * 0.9f
+        var prescale = PCA9685_25Mhz_TICKS / 4096f
+        prescale /= freq
+        prescale -= 1
+
+        val oldMode = i2c.readRegByte(PCA9685_MODE1)
+        val newMode = (oldMode and 0x7f) or 0x10 // sleep
+        i2c.apply {
+            println("new mode")
+            writeRegByte(PCA9685_MODE1, newMode) // go to sleep
+            println("prescale")
+            writeRegByte(PCA9685_PRESCALE, prescale.toByte()) // set prescale
+            println("old mode")
+            writeRegByte(PCA9685_MODE1, oldMode)
+//            Thread.sleep(5)
+//            writeRegByte(PCA9685_MODE1, oldMode or 0xa0.toByte()) // autoincrement
+        }
+    }
+
+    fun setPwmValue(on: Short, off: Short) {
+        i2c.apply {
+
+//            println("on $on, off $off")
+//            val s = "${value.toString(2)} = ${h.toString(2)}.${l.toString(2)}"
+//            println(s)
+
+//            val low = 0b01111111.toByte()
+//            val high = 0b11101100.toByte()
+//            writeRegByte(LED0_ON_H, 0)
+//            writeRegByte(LED0_ON_L, 0)
+//            writeRegByte(LED0_OFF_H, 0)
+//            writeRegByte(LED0_OFF_L, low)
+
+
+            val low = 103
+            val high = 492
+
+            val value = high
+            val value2 = low
+
+            val l = value and 0xFF
+            val h = value shr 8
+            val l2 = value2 and 0xFF
+            val h2 = value2 shr 8
+
+            writeRegByte(LED0_OFF_H, h.toByte())
+            writeRegByte(LED0_OFF_L, l.toByte())
+
+            writeRegByte(LED1_OFF_H, h2.toByte())
+            writeRegByte(LED1_OFF_L, l2.toByte())
+        }
+    }
+}
